0b4bac7ddbbc28a16862e0b87144c194ad8b8b9d
Connor Cooper <cooper.connor.17@gmail.com>
2024-07-15 16:41:08 +1000

    Neogit hotkey

--- config/neogit.nix
+++ config/neogit.nix
@@ -1,5 +1,15 @@
+# TODO: default config
 {
   plugins.neogit = {
     enable = true;
   };
+  keymaps = [
+    # Buffers
+    {
+      mode = "n";
+      key = "<leader>G";
+      action = "<cmd>Neogit<cr>";
+      options.desc = "Neogit";
+    }
+  ];
 }

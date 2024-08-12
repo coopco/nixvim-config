20a1d22f6ce496cc988bc33ed4b5731f95bb8327
Connor Cooper <cooper.connor.17@gmail.com>
2024-08-12 17:26:43 +1000

    vimtex

--- config/latex.nix
+++ config/latex.nix
@@ -1,6 +1,12 @@
+# TODO: fast snippets
+{ pkgs, ... }:
 {
   plugins = {
     vimtex = {
+      enable = true;
+      texlivePackage = pkgs.texlive.combined.scheme-full;
+    };
+    cmp-latex-symbols = {
       enable = true;
     };
   };
